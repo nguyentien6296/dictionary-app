c13258f0feb36c2bdeaa6b813690cdc786b581cd
nguyentien6296 <88523384+nguyentien6296@users.noreply.github.com>
2023-03-19 08:21:05 +0700

    up

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,11 @@
-import logo from "./c.png"
+import logo from "./c.png";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo}
-        className="App-logo img-fluid"
-        alt="logo"/>
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
     </div>
   );
